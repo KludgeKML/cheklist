7eaca25d4fd84090d2df81ce96cb96f2f0bb458d
Keith Lawrence <keith@kludge.co.uk>
2018-03-10 08:45:48 +0000

    derive from correct base class

--- app/controllers/api/v1/github_controller.rb
+++ app/controllers/api/v1/github_controller.rb
@@ -1,7 +1,7 @@
 # Handles incoming  hooks from GitHub
 module Api
   module V1
-    class Api::V1::GithubController < ApplicationController
+    class Api::V1::GithubController < ApiController
       def hook
         Rails.logger.info("Event-type: #{request.headers['X-GitHub-Event']}")
         
