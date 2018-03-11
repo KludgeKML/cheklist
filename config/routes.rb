c3b4ff7d337f2025b1db304365c3f57f449878ed
Keith Lawrence <keith@kludge.co.uk>
2018-03-11 11:28:47 +0000

    change hook to webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'github/hook', to: 'github#hook'
+      post 'github/webhook', to: 'github#webhook'
     end
   end
 end
