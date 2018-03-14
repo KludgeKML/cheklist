72e0863dea2411d4ecae15eccb6db76da2c6ee31
K M Lawrence <keith@kludge.co.uk>
2018-03-14 14:56:50 +0000

    import installed repositories

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,6 @@
 class SessionController < ApplicationController
   def create
+    byebug
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:current_user_id] = user.id
     redirect_to '/'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,29 @@ class User < ApplicationRecord
   def self.find_or_create_from_auth_hash(auth)
     where(display_name: auth.info[:nickname]).first_or_create do |user|
       user.display_name = auth.info[:nickname]
+      user.access_token = auth.credentials.token
+    end
+  end
+
+  def user_octokit_client
+    @user_octokit_client ||= Octokit::Client.new(access_token: access_token)
+  end
+
+  def list_available_installations
+    info = user_octokit_client.find_user_installations(accept: Octokit::Preview::PREVIEW_TYPES[:integrations])
+    info.installations
+  end
+
+  def list_repos_in_installation(installation_id)
+    info = user_octokit_client.find_installation_repositories_for_user(installation_id, accept: Octokit::Preview::PREVIEW_TYPES[:integrations])
+    info.repositories
+  end
+
+  def make_repo_records_from_installations
+    list_available_installations.each do |installation|
+      list_repos_in_installation(installation.id).each do |repo|
+        repositories.create(name: repo.full_name) unless repositories.where(name: repo.full_name).count > 0
+      end
     end
   end
 end
