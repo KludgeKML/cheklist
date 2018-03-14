e6329c659146699d1f2009eadede9367af82108a
K M Lawrence <keith@kludge.co.uk>
2018-03-14 10:45:18 +0000

    monkeypatch omniauth-github, fix view bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,19 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
+
+# Monkeypatch the github strategy so that it doesn't include
+# params in the redirect URL during the callback phase (which
+# causes Github to complain)
+
+require 'omniauth-github'
+
+module OmniAuth
+  module Strategies
+    class GitHub < OmniAuth::Strategies::OAuth2
+      def callback_url
+        full_host + script_name + callback_path
+      end
+    end
+  end
+end
